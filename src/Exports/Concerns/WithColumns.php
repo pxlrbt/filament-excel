b26fd180b67c580cde0a01380bbf6c30c4dea166
Dennis Koch <info@pixelarbeit.de>
2022-08-15 23:17:50 +0200

    Support `->toggleable()` for `->fromTable()`

--- src/Exports/Concerns/WithColumns.php
+++ src/Exports/Concerns/WithColumns.php
@@ -126,7 +126,9 @@ trait WithColumns
 
     protected function createFieldMappingFromTable(): Collection
     {
-        if ($this->getLivewire() instanceof HasTable) {
+        $livewire = $this->getLivewire();
+
+        if ($livewire instanceof HasTable) {
             $columns = collect(invade($this->getLivewire())->getTableColumns());
         } else {
             $table = $this->getResourceClass()::table(new Table());
@@ -134,6 +136,12 @@ trait WithColumns
         }
 
         return $columns
+            ->when(
+                $livewire->hasToggleableTableColumns(),
+                fn ($collection) => $collection->reject(
+                    fn (Tables\Columns\Column $column) => $livewire->isTableColumnToggledHidden($column->getName())
+                )
+            )
             ->mapWithKeys(function (Tables\Columns\Column $column) {
                 $invadedColumn = invade($column);
 
