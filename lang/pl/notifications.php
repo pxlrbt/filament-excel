71c515912ea5bd1c0b56d4c97e091837739e64d7
Kamil Stępień <kamilst96@gmail.com>
2022-10-24 23:57:03 +0200

    add pl translation

---
+++ lang/pl/notifications.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'queued' => [
+        'title' => 'Eksport rozpoczęty',
+        'body' => 'Eksport został dodany do kolejki. Zostaniesz powiadomiony, gdy będzie gotowy do pobrania.',
+    ],
+
+    'download_ready' => [
+        'title' => 'Eksport ukończony',
+        'body' => 'Twój plik eksportu jest gotowy do pobrania.',
+        'download' => 'Pobierz',
+    ],
+];
