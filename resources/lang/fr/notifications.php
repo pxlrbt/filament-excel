3361b0ac4d24d9c66110662c274b037c1653c433
becker <beckermaxime84@gmail.com>
2024-02-08 17:32:51 +0100

    add lang fr

---
+++ resources/lang/fr/notifications.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'queued' => [
+        'title' => 'Export en attente',
+        'body' => 'L\'exportation a été mise en attente. Vous serez notifié(e) lorsque celle-ci sera prête à être téléchargée.',
+    ],
+
+    'download_ready' => [
+        'title' => 'Export terminé',
+        'body' => 'Votre fichier est prêt à être téléchargé.',
+        'download' => 'Télécharger',
+    ],
+];
