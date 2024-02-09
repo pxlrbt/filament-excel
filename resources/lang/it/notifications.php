c11b1cbd1c73dbd4c31d57bcc85a1003b3a56855
becker <beckermaxime84@gmail.com>
2024-02-09 09:26:24 +0100

    add lang it

---
+++ resources/lang/it/notifications.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'queued' => [
+        'title' => 'Esportazione in coda',
+        'body' => "L'esportazione è stata messa in coda. Riceverai una notifica quando sarà pronta per il download.",
+    ],
+
+    'download_ready' => [
+        'title' => 'Esportazione completata',
+        'body' => 'Il tuo file è pronto per il download.',
+        'download' => 'Scarica',
+    ],
+];
