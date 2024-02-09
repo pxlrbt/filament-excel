1c78c78d76f0352aef3554b021633d77d964f386
becker <beckermaxime84@gmail.com>
2024-02-09 10:18:08 +0100

    add lang nl

---
+++ resources/lang/nl/notifications.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'queued' => [
+        'title' => 'Export in wachtrij',
+        'body' => 'De export is in de wachtrij gezet. U ontvangt een melding wanneer deze klaar is om te downloaden.',
+    ],
+
+    'download_ready' => [
+        'title' => 'Export voltooid',
+        'body' => 'Uw bestand is klaar om te downloaden.',
+        'download' => 'Downloaden',
+    ],
+];
