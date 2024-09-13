af36f933b032aefccc87d17431b6e74673b04af5
Dennis Koch <info@pixelarbeit.de>
2024-09-13 10:35:57 +0200

    security: fix CVE-2024-42485 for 1.x

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,17 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
+
 Route::get('filament-excel/{path}', function (string $path) {
+    $filename = substr($path, 37);
+    $path = Storage::disk('filament-excel')->path($path);
+
     return
         response()
-            ->download(Storage::disk('filament-excel')->path($path), substr($path, 37))
+            ->download($path, $filename)
             ->deleteFileAfterSend();
 })
+    ->middleware(['web', 'signed'])
     ->where('path', '.*')
     ->name('filament-excel-download');
