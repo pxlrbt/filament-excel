6e7b51b935218df5df8bb3320bb9fb8fd932db43
Berli Suharmanto <berli.suharmanto88@gmail.com>
2025-09-18 20:48:46 +0700

    chore(model): Simplified CanModifyModel trait - direct model instead of closures
    - Now directly set $model
    - Removed Closure to avoid potential queuing issues

--- src/Exports/Concerns/CanModifyModel.php
+++ src/Exports/Concerns/CanModifyModel.php
@@ -2,14 +2,9 @@
 
 namespace pxlrbt\FilamentExcel\Exports\Concerns;
 
-use Closure;
-use Laravel\SerializableClosure\SerializableClosure;
-
 trait CanModifyModel
 {
-  public ?SerializableClosure $modifyModelUsing = null;
-
-  public function modifyModelUsing(string|Closure $model): static
+  public function model(string $model): static
   {
     $this->model = $model;
 
